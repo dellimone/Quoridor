f6e87eec524378aa1d1fec4872974b704ec06a5e
Sara Trabucco <sara.trabucco99@gmail.com>
2026-02-10 22:05:49 +0100

    feat: added mockito setup + small test to see if mockito works (it does :) )

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,13 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    // for Mockito testing:
+    testImplementation("org.mockito:mockito-core:5.11.0")
+    testImplementation("org.mockito:mockito-junit-jupiter:5.11.0")
+
+    testImplementation("net.bytebuddy:byte-buddy:1.17.5")
+    testImplementation("net.bytebuddy:byte-buddy-agent:1.17.5")
 }
 
 tasks.test {
