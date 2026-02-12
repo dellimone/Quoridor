5f13d97aed89fb2b70c98e3432537f60c0004708
cristiano <cristiano.baldassi53@gmail.com>
2026-02-12 10:02:49 +0100

    fix: add test for player in not winning position, player1 wins at row 8 and player2 wins at row 0

--- src/test/java/it/units/quoridor/logic/QuoridorWinCheckerTest.java
+++ src/test/java/it/units/quoridor/logic/QuoridorWinCheckerTest.java
@@ -49,4 +49,44 @@ class QuoridorWinCheckerTest {
         // Assert
         assertFalse(hasWon);
     }
+
+    @Test
+    void player1WinsAtRow0 () {
+        // Arrange
+        Player player1 = new Player(PlayerId.PLAYER_1, "Alice", 10, 0);
+        Player player2 = new Player(PlayerId.PLAYER_2, "Bob", 10, 8);
+
+        Board board = new Board()
+                .withPlayerAt(PlayerId.PLAYER_1, new Position(0, 3)); // Player 1 at row 0
+
+        GameState state = new GameState(board, List.of(player1, player2));
+
+        WinChecker winChecker = new QuoridorWinChecker();
+
+        // Act
+        boolean hasWon = winChecker.isWin(state, PlayerId.PLAYER_1);
+
+        // Assert
+        assertTrue(hasWon);
+    }
+
+    @Test
+    void player2WinsAtRow8() {
+        // Arrange
+        Player player1 = new Player(PlayerId.PLAYER_1, "Alice", 10, 0);
+        Player player2 = new Player(PlayerId.PLAYER_2, "Bob", 10, 8);
+
+        Board board = new Board()
+                .withPlayerAt(PlayerId.PLAYER_2, new Position(8, 6)); // Player 2 at row 8
+
+        GameState state = new GameState(board, List.of(player1, player2));
+
+        WinChecker winChecker = new QuoridorWinChecker();
+
+        // Act
+        boolean hasWon = winChecker.isWin(state, PlayerId.PLAYER_2);
+
+        // Assert
+        assertTrue(hasWon);
+    }
 }
