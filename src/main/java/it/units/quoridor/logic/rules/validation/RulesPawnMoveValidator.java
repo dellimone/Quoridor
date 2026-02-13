58ab3819c8cd52a3bed48c6bd80b8e58f7cc39f1
Sara Trabucco <sara.trabucco99@gmail.com>
2026-02-13 14:52:56 +0100

    feat: pawn move validator correctly returns true when square is free from walls, false otherwise.

--- src/main/java/it/units/quoridor/logic/rules/validation/RulesPawnMoveValidator.java
+++ src/main/java/it/units/quoridor/logic/rules/validation/RulesPawnMoveValidator.java
@@ -1,12 +1,20 @@
 package it.units.quoridor.logic.rules.validation;
 
-import it.units.quoridor.domain.Direction;
-import it.units.quoridor.domain.GameState;
-import it.units.quoridor.domain.PlayerId;
+import it.units.quoridor.domain.*;
 
 public class RulesPawnMoveValidator implements PawnMoveValidator{
 
     public boolean canMovePawn(GameState state, PlayerId player, Direction direction) {
+
+        Board currentBoard = state.board();
+        Position currentPosition = state.getPlayerPosition(player);
+        Position proposedPosition = currentPosition.move(direction);
+
+        // we have to return true if the square in the proposed direction is free (from walls and players)
+        if (currentBoard.isEdgeBlocked(currentPosition, direction)) {
+            return false;
+        }
+
         return true;
     }
 }
--- src/test/java/it/units/quoridor/logic/validation/PawnValidatorTest.java
+++ src/test/java/it/units/quoridor/logic/validation/PawnValidatorTest.java
@@ -25,16 +25,40 @@ public class PawnValidatorTest {
     private final QuoridorGameRules rules = new QuoridorGameRules();
     private final PawnMoveValidator pawnValidator = new RulesPawnMoveValidator();
 
-    // 1. returns true when proposed square is free
+    // 1. returns true when proposed square is free from walls
     @Test
-    void returnsTrue_freeSquare() {
+    void returnsTrue_freeSquareFromWalls() {
         // create a small example for board
-        Board board = new Board();
         Player p1 = new Player(PlayerId.PLAYER_1, "P1", 10, 8);
         Player p2 = new Player(PlayerId.PLAYER_2, "P2", 10, 0);
+
+        Board board = new Board()
+                .withPlayerAt(PlayerId.PLAYER_1, new Position(0, 4))
+                .withPlayerAt(PlayerId.PLAYER_2, new Position(8, 4));
 
         GameState initialState = new GameState(board, List.of(p1, p2));
 
         assertTrue(pawnValidator.canMovePawn(initialState, PlayerId.PLAYER_1, Direction.EAST));
+    }
+
+    // 2. returns true when proposed square is blocked by walls
+    @Test
+    void returnsFalse_occupiedSquareByWall() {
+        // create a small example for board
+        Player p1 = new Player(PlayerId.PLAYER_1, "P1", 10, 8);
+        Player p2 = new Player(PlayerId.PLAYER_2, "P2", 10, 0);
+
+        Board board = new Board()
+                .withPlayerAt(PlayerId.PLAYER_1, new Position(0, 4))
+                .withPlayerAt(PlayerId.PLAYER_2, new Position(8, 4));
+
+        GameState initialState = new GameState(board, List.of(p1, p2));
+
+        WallPosition wallPosition = new WallPosition(0,4);
+        Wall wall = new Wall(wallPosition, WallOrientation.VERTICAL);
+        Board newBoard = initialState.board().addWall(wall);
+        GameState currentState = new GameState(newBoard, List.of(p1, p2));
+
+        assertFalse(pawnValidator.canMovePawn(currentState, PlayerId.PLAYER_1, Direction.EAST));
     }
 }
