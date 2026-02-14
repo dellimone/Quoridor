b92a93ac84d814ec0a1b89bf7a289ec8b5e07d75
Sara Trabucco <sara.trabucco99@gmail.com>
2026-02-14 18:18:32 +0100

    fix: fixed wrong validator usage in main; now demo works, wall placement works

--- src/main/java/it/units/quoridor/logic/validation/RulesWallPlacementValidator.java
+++ src/main/java/it/units/quoridor/logic/validation/RulesWallPlacementValidator.java
@@ -20,7 +20,6 @@ public class RulesWallPlacementValidator implements WallPlacementValidator{
     public boolean canPlaceWall(GameState state, PlayerId player, Wall wall) {
 
         Board board = state.board();
-
         Set<BlockedEdge> existingBlocked = board.getAllBlockedEdges();
 
         // compute edges blocked by new all and check if those are already blocked
