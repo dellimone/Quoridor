5927aaece46f8f110e6842f7157b2b1e6a8db331
Sara Trabucco <sara.trabucco99@gmail.com>
2026-02-14 16:47:37 +0100

    feat: added method in the QuoridorGameEngine for the controller to obtain valid pawn destinations given the current player

--- src/main/java/it/units/quoridor/engine/GameEngine.java
+++ src/main/java/it/units/quoridor/engine/GameEngine.java
@@ -2,6 +2,8 @@ package it.units.quoridor.engine;
 
 import it.units.quoridor.domain.*;
 
+import java.util.Set;
+
 public interface GameEngine {
 
     // only expose user-facing actions (to be furtherly expanded)
@@ -9,6 +11,7 @@ public interface GameEngine {
     MoveResult movePawn(PlayerId player, Direction direction);
     MoveResult movePawn(PlayerId player, Position position);
     MoveResult placeWall(PlayerId player, Wall wall);
+    Set<Position> legalPawnDestinationsForPlayer();
 
     void reset();
     boolean undo();
